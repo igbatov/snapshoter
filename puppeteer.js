ac9e7ab0aa9fe79c2c3de970cf78f79800814446
Batov <i.batov@megaplan.ru>
2018-02-16 15:54:20 +0300

    changed to ES6

--- puppeteer.js
+++ puppeteer.js
@@ -1,30 +1,20 @@
-require('babel-register');
 const puppeteer = require('puppeteer');
 
-function timeout(ms) {
+async function timeout(ms) {
     return new Promise(function(resolve) {
         setTimeout(resolve, ms)
     });
-};
+}
 
-var page = null
-var browser = null
-puppeteer.launch().then(function(b) {
-    browser = b;
-    return browser.newPage();
-}).then(function(p){
-    page = p;
-    return page.setViewport({width: 5960, height: 4209});
-}).then(function(p) {
-    return page.goto('http://help.local/help/article/tasks/list', {waitUntil: 'networkidle2'});
-}).then(function(page) {
-    return timeout(10000);
-}).then(function(p) {
-    return page.evaluate(function() {
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('http://help.local/help/article/tasks/list', {waitUntil: 'networkidle2'});
+    await timeout(10000);
+    await page.screenshot({path: 'example.png'});
+    const html = await page.evaluate(function() {
         return new XMLSerializer().serializeToString(document.doctype) + document.documentElement.outerHTML;
     })
-    //return page.screenshot({path: 'example.png'});
-}).then(function(html){
     console.log(html)
-    browser.close();
-});
+    await browser.close();
+})();
